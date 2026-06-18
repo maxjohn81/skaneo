4e08c9a55b4f7bcb12ab04159230bc714745c7dc
maxjohn81 <rivoandriharisoa@gmail.com>
2026-06-18 12:21:02 +0300

    oui

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,12 @@
+import { Tabs } from 'expo-router';
+
+export default function TabLayout() {
+ return (
+  <Tabs
+   tabBar={() => null}
+   screenOptions={{ headerShown: false }}
+  >
+   <Tabs.Screen name="index" />
+  </Tabs>
+ );
+}
