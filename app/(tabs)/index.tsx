c89c8bb0b1d37189c787106de02f8a6d4c9b4276
maxjohn81 <rivoandriharisoa@gmail.com>
2026-06-29 23:44:25 +0300

    oui

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
 import { Ionicons } from "@expo/vector-icons";
 import { router } from "expo-router";
 import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   const handleScan = () => {
@@ -8,35 +9,47 @@ export default function HomeScreen() {
   };
 
   return (
-    <View style={styles.container}>
-      <View style={styles.logoContainer}>
-        <View style={styles.logo}>
-            <Image
-              source={require('../../assets/images/skaneo.png')}
-              style={{ width: 200, height: 200 }}
-            />
+    <>
+      <SafeAreaView style={{flex:1}}>
+        <View style={styles.container}>
+          <View style={styles.logoContainer}>
+            <View style={styles.logo}>
+              <Image
+                source={require('@/assets/images/icon_skaneo.png')}
+                style={{ width: 200, height: 200, position: "absolute", left: -215, top: -45 }}
+              />
+              <Text style={styles.slogan}>kaneo</Text>
+            </View>
+
+            <Text style={styles.title}>
+              Scanner votre carte à gratter
+            </Text>
+            <Text style={styles.title}>
+              <Text style={styles.yas}>Yas, </Text>
+              <Text style={styles.orange}>Orange, </Text>
+              <Text style={styles.airtel}>Airtel</Text>
+            </Text>
+          </View>
+
+          <TouchableOpacity style={styles.scanButton} onPress={handleScan}>
+            <Ionicons name="camera" size={20} color="white" />
+            <Text style={styles.scanButtonText}>Scanner une carte</Text>
+          </TouchableOpacity>
+
+          <View style={styles.footer}>
+            <Text style={styles.footerText}>
+              © 2026 Skaneo. Tous droits réservés.
+            </Text>
+          </View>
         </View>
-        <Text style={styles.slogan}>Scanner votre carte crédit</Text>
-      </View>
-
-      <TouchableOpacity style={styles.scanButton} onPress={handleScan}>
-        <Ionicons name="camera" size={20} color="white" />
-        <Text style={styles.scanButtonText}>Scanner une carte</Text>
-      </TouchableOpacity>
-
-      <View style={styles.footer}>
-        <Text style={styles.footerText}>
-          © 2024 Skaneo. Tous droits réservés.
-        </Text>
-      </View>
-    </View>
+      </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#0A0F1E",
     justifyContent: "space-between",
     alignItems: "center",
     paddingVertical: 60,
@@ -49,33 +62,44 @@ const styles = StyleSheet.create({
   },
 
   logo: {
-    fontSize: 80,
     borderRadius: 50,
-    backgroundColor: "#0EA5E9",
-    // padding: 20,
-    overflow: "hidden",
+    position: 'relative'
   },
 
   title: {
-    fontSize: 42,
+    top: 200,
+    fontSize: 18,
     fontWeight: "bold",
-    color: "#FFFFFF",
+    // color: "#FFFFFF",
     marginTop: 10,
   },
 
   slogan: {
-    fontSize: 18,
-    color: "#7DD3FC",
+    position: 'absolute',
+    top: 0,
+    left: -72,
+    fontSize: 100,
     marginTop: 10,
+    fontWeight: "bold"
+  },
+  yas: {
+    color: "#FFBF00",
+  },
+  orange: {
+    color: "#FF4400",
+  },
+  airtel: {
+    color: "#FF0000",
   },
 
   scanButton: {
+    top: 100,
     display: "flex",
     flexDirection: "row",
     justifyContent: "center",
     gap: 10,
     width: "100%",
-    backgroundColor: "#0EA5E9",
+    backgroundColor: "#FFBF00",
     paddingVertical: 18,
     borderRadius: 16,
     alignItems: "center",
